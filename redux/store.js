bbacd49eb5e1776b132c01a23b860a7255d883ff
Tlalemasego <tlalemasego@gmail.com>
2024-07-26 15:18:45 +0200

    git commit -  replaced my class with a function that creates a store with reducer and initialState as parameters

--- redux/store.js
+++ redux/store.js
@@ -1,32 +1,35 @@
 // Importing the reducer function 
 import reducer  from "./reducer.js";
 
-// store class defined to managed the state and handle subscription and dispatches 
-class Store {
-    constructor(reducer, initialState) {  
-        this.reducer = reducer;
-        this.state = initialState;
-        this.listeners = [];
+// This function creates a store with reducer and initialState as its parameters
+function myStore(reducer, initialState) {
+    let state = initialState;  //The use of let allows the state to be updated 
+    const listeners= new Set(); //A Set that will hold functions to be called when the stage changes
+
+    function getState() {
+        return state;  // Returns the current state
     }
 
-    getState() {  // This method returns the current state of the store 
-        return this.state;
+    function dispatch(action) {
+        state = reducer(state, action);  // Dispatches and action and uses the reducer to update the state
+        listeners.forEach(listener => listener()); //Subscribed listeners are notified
     }
 
-    dispatch(action) {   // The action object that calls the reducer function, updates the state with the result of the reducer and notifies listerners
-        this.state = this.reducer(this.state, action);
-        this.listeners.forEach(listener => listener());
-    }
-
-    subscribe(listener) { // The listeners function is called whenever the state changes
-        this.listeners.push(listener);
-        return () => {
-            this.listeners = this.listeners.filter(l => l !== listener); // Arrow function used as the test function filter, 
+    function subscribe(listener) { // Adds a listener function to the listeners set 
+        listeners.add(listener);
+        return() => { //Returns a function that removes listener from the Set when called 
+            listeners.delete(listener);
         };
     }
+    return{
+        getState,
+        dispatch,
+        subscribe
+    };
 }
+
 // Declaring a new instance of the store class with the reducer and initial state of 0
-const store = new Store(reducer, { count: 0 });
+const store = myStore(reducer, { count: 0 });
 
 
 export default store; 
